6817fadf055f9eaad182a730c1f7905915bfe114
tsstinso <tsstinso@iu.edu>
2016-04-14 23:33:35 -0400

    created the findMinCost function and began passing variables into it.

--- matrixMultiplicationOrdering/minMatrixMultiplicationWork.cpp
+++ matrixMultiplicationOrdering/minMatrixMultiplicationWork.cpp
@@ -13,31 +13,45 @@
 #include <fstream>
 using namespace std;
 
+int findMinCost(const int m[], int size)
+//function finds the minimum cost of matrix multiplications
+{
+	const int n = 10;
+	int bigM[n][n]; //represents the 2d nxn table
 
+
+
+}
 
 int main()
 //main function of program
 //connects to input file and populates array a with input from matrix.txt
 
-{
 
+{
+	int m[11]; //declare an array of size 11. 11 is the max size of the array
+	int n; //this will be the array elements from the input file
+	int i = 0; // array index varriable
+
+	//access datafile and read in elements, eliments in datafile are on seperate lines
 	ifstream in_stream;
-	in_stream.open("peak.txt");
+	in_stream.open("matrix.txt");
 	if (in_stream.fail())
 	{
 		cout << "Error: unable to find input file";  //error message
 		exit(1);  //exits the program
 	}//end if
 
-	while (in_stream >> n)
+	while (in_stream >> n) //fill in the array
 	{
-		a[i++] = n;
+		m[i++] = n;
 	}
 
 	//read the file – see below
 	in_stream.close();
+	findMinCost(m, i-1);
 
-	p = peak(a, start, i - 1);
+	/* will need this to output the results
 	ofstream out_stream;
 	out_stream.open("peak.txt", ios::app);
 	if (out_stream.fail())
@@ -46,7 +60,7 @@ int main()
 		exit(1); //exit the program
 	}//end if
 	out_stream << "the peak value is, " << p;
-
+	*/
 	return 0;
 
 }
