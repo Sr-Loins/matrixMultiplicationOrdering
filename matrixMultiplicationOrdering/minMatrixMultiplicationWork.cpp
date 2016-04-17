283c35e17adf9c6caa9af791ca3fe0b0ba1b1945
tsstinso <tsstinso@iu.edu>
2016-04-17 17:04:04 -0400

    everything works

--- matrixMultiplicationOrdering/minMatrixMultiplicationWork.cpp
+++ matrixMultiplicationOrdering/minMatrixMultiplicationWork.cpp
@@ -11,8 +11,25 @@
 ******************************************************************/
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <string>
 using namespace std;
+
+void printParin(ofstream &out_stream, int kArray[10][10], int i, int j)
+{
+
+	if (i == j)
+	{
+		out_stream << "A"<<i<<" ";
+	}
+	else
+	{
+		out_stream << "(";
+		printParin(out_stream, kArray, i, kArray[i][j]);
+		printParin(out_stream, kArray, kArray[i][j] + 1, j);
+		out_stream << ")";
+	}
+	
+}
 
 int findMinCost(const int m[], int n)
 //function finds the minimum cost of matrix multiplications by traversing the 
@@ -26,6 +43,7 @@ int findMinCost(const int m[], int n)
 	//std::vector< std::vector<int> > bigM; //represents the 2d nxn table
 	//bigM.resize(size, vector<int>(size));
 	int bigM[10][10]; //represents the 2d nxn table
+	int kArray[10][10]; // array that holds the k split value
 	for (int i = 0; i <= n; i++)//set the first row and first column to 0
 	{
 		bigM[i][0] = 0;
@@ -53,12 +71,22 @@ int findMinCost(const int m[], int n)
 				if (min < bigM[i][j])//assign the min operation cost to bigM[i][j]
 				{
 					bigM[i][j] = min;
+					kArray[i][j] = k;
 				}//end if
 			}//end k for
 		}//end i for
 	}//end d for 
 	min = bigM[1][n - 1];
-
+	ofstream out_stream;
+	out_stream.open("matrix.txt", ios::app);
+	if (out_stream.fail())
+	{
+		cout << "Error: unable to find output file"; //error message
+		exit(1); //exit the program
+	}//end if
+	printParin(out_stream, kArray, 1, n - 1);
+	out_stream << endl; 
+	out_stream.close();
 	return min;
 }
 
@@ -71,10 +99,15 @@ int main()
 	//std::vector<int> m; //declare a vector
 	//m.reserve(10); //make room for 10 elements
 	int m[10]; //declare an array of size 10. 10 is the max size of the array
+	string matrix[10]; // array of the matrix symbols
+	//char symbol;
+	//string line;
 	int n; //this will be the array elements from the input file
 	int i = 0; // array index varriable
+	int p = 0; // matrix array index varriable
 	int size = 0; //keeps track of the size of the array
 	int min = 0;
+	//int kArray[10][10]; //keeps track of k value
 	//access datafile and read in elements, eliments in datafile are on seperate lines
 	ifstream in_stream;
 	in_stream.open("matrix.txt");
@@ -92,11 +125,22 @@ int main()
 		m[i] = n;
 		i++;
 	}
+	//p = i;
+/*	
+	while ( getline (in_stream, line))
+	{
+		//in_stream.get(symbol);
+		matrix[p] = line;
+		p++;
+		//in_stream.get(symbol);
+		//p++;
+	}
+	*/
 	//cout << m.size();//debugging just to see what the size of the vector is
 	//read the file – see below
 	in_stream.close();
 	//size = i;
-	min = findMinCost(m, i); //pass vector and size to findMinCost
+	min = findMinCost(m, i); //pass array and size to findMinCost
 
 	
 	ofstream out_stream;
@@ -107,7 +151,7 @@ int main()
 		exit(1); //exit the program
 	}//end if
 	out_stream << "the peak value is, " << min;
-
+	out_stream.close();
 	return 0;
 
 }
